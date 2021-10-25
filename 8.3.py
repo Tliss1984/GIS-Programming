05169e362776b311f88488a3ad79cb1003b3ea1a
Tliss1984 <tliss1984@aim.com>
2021-10-25 01:50:10 -0400

    Create 8.3.py

---
+++ 8.3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def is_palindrome(palin):
+
+    return palin == palin[::-1]
+#tests to see if a word is the same as its reverse
+
+print (is_palindrome("dog god"))
+
+
+# In[ ]:
+
+
+
+
