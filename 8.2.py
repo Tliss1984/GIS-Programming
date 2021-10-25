9ea030152481448f2cc279f2ee6b7afceb3ea10f
Tliss1984 <tliss1984@aim.com>
2021-10-25 01:55:52 -0400

    Create 8.2.py

---
+++ 8.2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#assigns "banana to a variable
+letters = 'banana'
+#assigs the amount of letters are in the previous variable to a new variable
+how_many = letters.count('a')
+#prints the new variable
+print (how_many)
+
+
+# In[ ]:
+
+
+
+
